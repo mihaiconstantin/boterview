ab093ab4daf4e745a3e747b6440d2edd0785e893
mihaiconstantin <mihai@mihaiconstantin.com>
2025-02-11 13:28:07 +0100

    Feat: add `Guide` class for interview guide

---
+++ src/boterview/backend/guide.py
@@ -0,0 +1,25 @@
+# Imports.
+from .printable import Printable
+from .helpers import read_contents
+
+
+# `Guide` class for interview background information.
+class Guide(Printable):
+    # The interview guide text.
+    text: str = ""
+
+    # Initialize the guide.
+    def __init__(self: "Guide", file: str):
+        # Read and set the text from the file.
+        self.text = read_contents(file)
+
+    # Prepare text.
+    def to_text(self: "Guide") -> str:
+        # Prepare the output text.
+        output: str = "## Interview Guide\n\n"
+
+        # Add the text.
+        output += self.text
+
+        # Return the string.
+        return output
