e0a8dbd0271998825648468fb0c3eedd41f76084
mihaiconstantin <mihai@mihaiconstantin.com>
2025-02-11 13:28:38 +0100

    Feat: add `Closing` class for interview closing instructions

---
+++ src/boterview/backend/closing.py
@@ -0,0 +1,25 @@
+# Imports.
+from .printable import Printable
+from .helpers import read_contents
+
+
+# `Closing` class for interview closing.
+class Closing(Printable):
+    # The interview closing text.
+    text: str = ""
+
+    # Initialize the closing.
+    def __init__(self: "Closing", file: str):
+        # Read and set the text from the file.
+        self.text = read_contents(file)
+
+    # Prepare text.
+    def to_text(self: "Closing") -> str:
+        # Prepare the output text.
+        output: str = "## Interview Closing\n\n"
+
+        # Add the text.
+        output += self.text
+
+        # Return the string.
+        return output
