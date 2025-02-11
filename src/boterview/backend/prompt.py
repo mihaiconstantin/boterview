1c90658b695d3a22e1d82d6037754142097f3167
mihaiconstantin <mihai@mihaiconstantin.com>
2025-02-11 13:32:01 +0100

    Feat: add `Prompt` class for model system prompts

---
+++ src/boterview/backend/prompt.py
@@ -0,0 +1,19 @@
+# Imports.
+from .printable import Printable
+from .helpers import read_contents
+
+
+# `Prompt` class for model.
+class Prompt(Printable):
+    # The model system prompt text.
+    text: str = ""
+
+    # Initialize the prompt.
+    def __init__(self: "Prompt", file: str):
+        # Read and set the text from the file.
+        self.text = read_contents(file)
+
+    # Prepare text.
+    def to_text(self: "Prompt") -> str:
+        # Return the string.
+        return self.text
