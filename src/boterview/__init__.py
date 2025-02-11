0fa910a9b16ad4c764ed50ac4951c71a885ed010
mihaiconstantin <mihai@mihaiconstantin.com>
2025-02-11 13:33:43 +0100

    Feat: group package-level imports for ease of use

--- src/boterview/__init__.py
+++ src/boterview/__init__.py
@@ -1,2 +1,31 @@
 # Imports for easier client usage.
-from .helpers import test
+
+# Helpers.
+from .backend.helpers import parse_questions
+from .backend.helpers import read_contents
+
+# Interfaces.
+from .backend.printable import Printable
+
+# Classes.
+
+# Interview classes.
+from .backend.protocol import Protocol
+from .backend.question import Question
+from .backend.guide import Guide
+from .backend.introduction import Introduction
+from .backend.closing import Closing
+from .backend.interview import Interview
+
+# Model classes.
+from .backend.prompt import Prompt
+
+# Study classes.
+from .backend.study import Study
+from .backend.condition import Condition
+from .backend.conversation import Conversation
+from .backend.participant import Participant
+from .backend.counter import Counter
+
+# Application classes.
+from .backend.boterview import Boterview
