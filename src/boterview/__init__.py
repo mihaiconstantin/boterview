09cc88227d8e64f118eb458909f78d44a41fc6cc
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-06 22:36:58 +0100

    Refactor: update source root `__init__.py` to expose main classes

--- src/boterview/__init__.py
+++ src/boterview/__init__.py
@@ -1,32 +1,12 @@
-# Imports for easier client usage.
+# Imports.
+from boterview.services.configuration.configuration import Configuration
+from boterview.services.ui.ui import UI
+from boterview.services.boterview.boterview import Boterview
 
-# Helpers.
-from boterview.backend.helpers import parse_questions
-from boterview.backend.helpers import read_contents
 
-# Interfaces.
-from boterview.backend.printable import Printable
-
-# Classes.
-
-# Interview classes.
-from boterview.backend.protocol import Protocol
-from boterview.backend.question import Question
-from boterview.backend.guide import Guide
-from boterview.backend.introduction import Introduction
-from boterview.backend.closing import Closing
-from boterview.backend.interview import Interview
-
-# Model classes.
-from boterview.backend.prompt import Prompt
-
-# Study classes.
-from boterview.backend.study import Study
-from boterview.backend.condition import Condition
-from boterview.backend.conversation import Conversation
-from boterview.backend.participant import Participant
-from boterview.backend.counter import Counter
-
-# Application classes.
-from boterview.backend.boterview import Boterview
-from boterview.backend.configuration import Configuration
+# Expose the following modules.
+__all__ = [
+    "Configuration",
+    "UI",
+    "Boterview"
+]
