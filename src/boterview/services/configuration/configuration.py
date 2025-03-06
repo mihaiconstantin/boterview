8d85afe010832dc066cf7240807544e396f9af0c
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-06 21:09:02 +0100

    Refactor: update check for configuration format

--- src/boterview/services/configuration/configuration.py
+++ src/boterview/services/configuration/configuration.py
@@ -85,28 +85,62 @@ class Configuration:
             # Check if it is present.
             assert section in self.data, f"Missing configuration section \"{ section }\"."
 
-    # Check the required configuration format.
+    # Set optional keys to null if values are not provided.
     def _validate_configuration_format(self: "Configuration") -> None:
         # For each configuration section in the expected format.
-        for section in self.configuration_format:
+        for section, section_type in self.configuration_sections.items():
 
-            # For each expected key in the current section.
-            for key in self.configuration_format[section].keys():
+            # If the section is optional and not provided in the configuration file.
+            if utils.is_optional(section_type) and section not in self.data:
+                # Set the section to an empty dictionary.
+                self.data[section] = {}
+
+            # For each key in the current section.
+            for key, key_type in self.configuration_format[section].items():
+
+                # If the key is optional and not provided in the configuration file.
+                if utils.is_optional(key_type) and key not in self.data[section]:
+                    # If the type is a dictionary.
+                    if utils.is_dictionary(key_type):
+                        # Set the key to an empty dictionary.
+                        self.data[section][key] = {}
+                    else:
+                        # Set the key to `None`.
+                        self.data[section][key] = None
 
                 # Assert that the key is present in the parsed configuration.
-                assert key in self.data[section], f"Missing configuration key '{ section }.{ key }'."
+                assert key in self.data[section], f"Missing configuration key \"{ section }.{ key }\"."
 
-                # If the key happens to be the `conditions` list.
+                # If the key belongs to the conditions list.
                 if key == "conditions":
 
-                    # For each condition key in the expected format.
-                    for condition_key in self.configuration_format[section][key].keys():
+                    # For each condition provided in the configuration.
+                    for condition in self.data[section][key]:
 
-                        # Assert that the condition key is present in the parsed configuration for each condition.
-                        for condition in self.data[section][key]:
+                        # For each condition key in the conditions list of keys.
+                        for condition_key, condition_key_type in self.configuration_format[section][key].items():
+
+                            # If the key is optional and not provided in the configuration file for the current condition.
+                            if utils.is_optional(condition_key_type) and condition_key not in condition:
+                                # Set the key to `None`.
+                                condition[condition_key] = None
 
                             # Assert that the condition key is present in the current condition.
-                            assert condition_key in condition, f"Missing configuration key '{ section }.{ key }.{ condition_key }'."
+                            assert condition_key in condition, f"Missing configuration key \"{ section }.{ key }.{ condition_key }\"."
+
+                # If the key belongs to the `ui` dictionary.
+                if key in self.configuration_format["ui"].keys() and not utils.is_optional(key_type):
+
+                    # For each `ui` key in the expected format.
+                    for ui_key, ui_key_type in self.configuration_format[section][key].items():
+
+                        # If the key is optional and not provided in the configuration file.
+                        if utils.is_optional(ui_key_type) and ui_key not in self.data[section][key]:
+                            # Set the key to `None`.
+                            self.data[section][key][ui_key] = None
+
+                        # Assert that the `ui` key is present in the parsed configuration.
+                        assert ui_key in self.data[section][key], f"Missing configuration key \"{ section }.{ key }.{ ui_key }\"."
 
     # Parse the `API` key.
     def _parse_api_key(self: "Configuration") -> None:
