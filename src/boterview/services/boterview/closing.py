23e6a1b73158f347bbb9c7da1bcf119afa80c132
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-06 14:38:30 +0100

    Refactor: adjust `Closing` text representation when lacking content

--- src/boterview/services/boterview/closing.py
+++ src/boterview/services/boterview/closing.py
@@ -27,7 +27,12 @@ class Closing(Printable):
 
     # Prepare text.
     def to_text(self: "Closing") -> str:
-        # Prepare the output text.
+        # If the text is empty, return an empty string.
+        if not self.text:
+            # Return
+            return ""
+
+        # Otherwise, prepare the output text.
         output: str = "## Interview Closing\n\n"
 
         # Add the text.
