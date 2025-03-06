721f88a517f10a5756a91171a891166d4b9a7705
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-06 22:54:38 +0100

    Feat: add `generate codes` command to generate participation codes

--- src/boterview/cli/commands/generate.py
+++ src/boterview/cli/commands/generate.py
@@ -1,5 +1,9 @@
 # Imports.
+from typing import List
 import click
+
+# Import helpers.
+import boterview.helpers.utils as utils
 
 
 # Create a command group for generate.
@@ -8,3 +12,30 @@ def generate():
     """Commands to generate various things."""
     pass
 
+
+# Subcommand for generating participation codes.
+@generate.command(name = "codes")
+@click.option("-q", "--quantity", type = int, required = True, help = "The amount of participation codes to generate.")
+@click.option("-f", "--file", type = str, required = False, help = "The file to write the codes to.")
+def codes(quantity: int, file: str):
+    """Command to generate participation codes."""
+
+    # Generate six letter codes only from letters and digits.
+    codes: List[str] = utils.generate_codes(quantity)
+
+    # Prepare the contents.
+    contents: str = "\n".join(codes)
+
+    # If the file is missing, the only print the codes.
+    if file is None:
+        # Print the codes.
+        click.echo(contents)
+
+        # Return.
+        return
+
+    # Otherwise, open the file.
+    utils.write_contents(file, contents)
+
+    # Print a success message.
+    click.echo(f"Generated { quantity } codes written to '{ file }'.")
