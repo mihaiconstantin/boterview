4a11b5b0748a023d93e70f0ae74a463743604d69
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-06 22:40:26 +0100

    Refactor: disable traceback in CLI to simplify error output

--- src/boterview/cli/cli.py
+++ src/boterview/cli/cli.py
@@ -1,9 +1,13 @@
 # Imports.
 import click
+import sys
 
 # Import the commands.
 from boterview.cli.commands.preview import preview
 from boterview.cli.commands.run import run
+
+# Disable traceback.
+sys.tracebacklimit = 0
 
 
 # The CLI group for the application commands.
