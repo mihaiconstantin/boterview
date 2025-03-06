48e81f7498e2b7cf677a1fa58b938ef98e9c07db
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-06 22:59:22 +0100

    Feat: add `generate study` command to scaffold a study from template

--- src/boterview/cli/commands/generate.py
+++ src/boterview/cli/commands/generate.py
@@ -1,6 +1,11 @@
 # Imports.
 from typing import List
+import pathlib
 import click
+from boterview.services.configuration.configuration import Configuration
+
+# Import the content.
+from boterview.context.content import CONTENT
 
 # Import helpers.
 import boterview.helpers.utils as utils
@@ -54,3 +59,91 @@ def secret():
         f"Generated secret: {secret}\n"
         "You may copy this secret to your configuration file."
     )
+
+
+# Subcommand for an example study setup.
+@generate.command(name = "study")
+@click.option("-p", "--path", type = str, required = True, help = "The path to scaffold the study setup.")
+@click.option("-q", "--quantity", type = int, required = True, help = "The amount of participation codes to generate.")
+@click.option("-c", "--config", type = str, required = False, default = "study.toml", help = "The name of the configuration file.")
+def study(path: str, quantity: int, config: str):
+    """Command to scaffold an example study setup at the path provided."""
+
+    # Define the the scaffolding location.
+    location: pathlib.Path = pathlib.Path(path)
+
+    # Create a configuration instance.
+    configuration: Configuration = Configuration()
+
+    # Write the configuration file.
+    utils.create_and_write_contents(
+        file = location / f"{config}",
+        contents = configuration.template.to_toml(directory = location)
+    )
+
+    # Prepare the list of files to write.
+    configuration_files = {
+        # The UI files.
+        "ui": {key for key in configuration.configuration_format["ui"]},
+
+       # The study files.
+        "study": {
+            "codes": {},
+            "conditions": {key for key in configuration.configuration_format["study"]["conditions"] if key != "name"}
+        }
+    }
+
+    # For each section that has files to be written.
+    for section in configuration_files:
+
+        # For setting in the respective section.
+        for setting in configuration_files[section]:
+
+            # If we are dealing with UI files.
+            if section == "ui":
+                # Determine the path to write at.
+                file = location / f"{configuration.template.get(f"{section}.{setting}.content")}"
+
+                # Determine the content.
+                contents = str(CONTENT[section][setting])
+
+                # Write the file.
+                utils.create_and_write_contents(file, contents)
+
+                # Continue to the next iteration.
+                continue
+
+            # If we are dealing with study files.
+            if section == "study":
+                # If we are dealing with condition files.
+                if setting == "conditions":
+                    # For each condition.
+                    for condition in configuration_files[section][setting]:
+                        # Determine the path to write at.
+                        file = location / f"{configuration.template.get(f"{section}.{setting}.{condition}")}"
+
+                        # Determine the content.
+                        contents = CONTENT[section][setting][condition]
+
+                        # Write the file.
+                        utils.create_and_write_contents(file, contents)
+
+                # Otherwise, it's a study-level file.
+                else:
+                    # Determine the path to write at.
+                    file = location / f"{configuration.template.get(f"{section}.{setting}")}"
+
+                    # If the setting is codes, generate the codes.
+                    if setting == "codes":
+                        # Generate the codes.
+                        contents = "\n".join(utils.generate_codes(quantity))
+
+                    # Otherwise, get the content from the default contents.
+                    else:
+                        contents = str(CONTENT[section][setting])
+
+                    # Write the file.
+                    utils.create_and_write_contents(file, contents)
+
+    # User feedback.
+    click.echo(f"Study setup scaffolded. The configuration file is available at \"{location / config}\".")
