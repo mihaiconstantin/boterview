218a36ec606321680e3979279258f4e26fcf1b84
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-06 14:41:15 +0100

    Refactor: update `Introduction` to parse file input in a separate method

--- src/boterview/services/boterview/introduction.py
+++ src/boterview/services/boterview/introduction.py
@@ -10,10 +10,20 @@ class Introduction(Printable):
     # The interview introduction text.
     text: str = ""
 
+    # Parse the guide text.
+    def _parse_file(self: "Introduction", file: str | None) -> None:
+        # If the file is not set.
+        if not file:
+            # Return
+            return
+
+        # Otherwise, read and set the text from the file.
+        self.text = utils.read_contents(file)
+
     # Initialize the introduction.
-    def __init__(self: "Introduction", file: str):
-        # Read and set the text from the file.
-        self.text = read_contents(file)
+    def __init__(self: "Introduction", file: str | None):
+        # Parse the text.
+        self._parse_file(file)
 
     # Prepare text.
     def to_text(self: "Introduction") -> str:
