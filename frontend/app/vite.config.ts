062d4dab4de517f928e8402c5117f387e33cdb3b
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-09 21:13:05 +0100

    Build: add frontend configuration files

---
+++ frontend/app/vite.config.ts
@@ -0,0 +1,40 @@
+// Imports.
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+
+// See `https://vite.dev/config`.
+export default defineConfig(({ mode }) => {
+    // Load the environment variables in the current mode.
+    const env = loadEnv(mode, ".", "BOTERVIEW_");
+
+    // Return the configuration.
+    return {
+        // Enable plugins.
+        plugins: [
+            react(),
+            tailwindcss()
+        ],
+
+        // Configure the development server.
+        server: {
+            // Frontend development server port.
+            port: 5173,
+
+            // Proxy.
+            proxy: {
+                '/api': {
+                    target: env.BOTERVIEW_BACKEND_URL,
+                    changeOrigin: true,
+                    secure: false
+                },
+                '/chat': {
+                    target: env.BOTERVIEW_BACKEND_URL,
+                    changeOrigin: true,
+                    secure: false
+                }
+            }
+        }
+    }
+});
