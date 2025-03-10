de8002fcf702a040f8a3e169cc2d4237ec83abf0
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-10 12:37:05 +0100

    Feat: add storage helper to save `Participant` to database

--- src/boterview/helpers/storage.py
+++ src/boterview/helpers/storage.py
@@ -1,4 +1,5 @@
 # Imports.
+from boterview.services.boterview.participant import Participant
 from boterview.models.database.participant import Participant as ParticipantModel
 
 
@@ -9,3 +10,18 @@ def get_participant(code: str) -> ParticipantModel:
 
     # Return the participant.
     return participant
+
+
+# Save participant to the sqlite database.
+def save_participant(participant: Participant) -> ParticipantModel:
+    # Create a participant model and insert the record into the database.
+    participant_model: ParticipantModel = ParticipantModel.create(
+        code = participant.code,
+        consent = participant.consent,
+        start_time = participant.start_time,
+        condition = participant.condition_name,
+        prompt = participant.prompt
+    )
+
+    # Return the created participant record.
+    return participant_model
