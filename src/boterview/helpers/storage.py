e0bccfe30ed307847a8ad4cc6b1f564c7dea918b
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-10 12:38:57 +0100

    Feat: add storage helper to save a conversation to the database

--- src/boterview/helpers/storage.py
+++ src/boterview/helpers/storage.py
@@ -1,6 +1,7 @@
 # Imports.
 from boterview.services.boterview.participant import Participant
 from boterview.models.database.participant import Participant as ParticipantModel
+from boterview.models.database.conversation import Conversation as ConversationModel
 
 
 # Get the participant from the database by code.
@@ -25,3 +26,16 @@ def save_participant(participant: Participant) -> ParticipantModel:
 
     # Return the created participant record.
     return participant_model
+
+
+# Save conversation to the sqlite database.
+def save_conversation(participant_model: ParticipantModel, message_type: str, message: str) -> ConversationModel:
+    # Create a conversation model and insert the record into the database.
+    conversation_model: ConversationModel = ConversationModel.create(
+        participant = participant_model,
+        message_type = message_type,
+        message = message
+    )
+
+    # Return the created conversation record.
+    return conversation_model
