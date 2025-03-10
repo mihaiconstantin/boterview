a4d5108a617cbfaad50b846f0a57f455ea872147
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-10 11:58:40 +0100

    Feat: add custom `UTCDateTimeField` `peewee` field

--- src/boterview/models/database/base.py
+++ src/boterview/models/database/base.py
@@ -1,5 +1,6 @@
 # Imports.
-from peewee import Model
+from datetime import datetime
+from peewee import Model, DateTimeField
 
 # Import the context.
 import boterview.context.persistence as persistence
@@ -10,3 +11,23 @@ class BaseModel(Model):
     """Base model for the database."""
     class Meta:
         database = persistence.database
+
+
+# Define a custom `DateTimeField` for UTC timestamps (i.e., ISO 8601 compliant).
+class UTCDateTimeField(DateTimeField):
+    # Convert the `Python` value to the database value.
+    def python_value(self, value):
+
+        # Use the default implementation.
+        value = super().python_value(value)
+
+        # If it is a string.
+        if isinstance(value, str):
+            # Convert it to a `datetime` object.
+            date: datetime = datetime.fromisoformat(value)
+
+            # Return the converted string.
+            return date
+
+        # Otherwise, return the value.
+        return value
