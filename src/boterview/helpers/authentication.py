0df58a123681d4c94301a5eefb8c0843b0dccec5
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-10 11:44:46 +0100

    Feat: add helper to create `JWT`

--- src/boterview/helpers/authentication.py
+++ src/boterview/helpers/authentication.py
@@ -1,5 +1,7 @@
 # Imports.
-from typing import Dict
+from typing import Callable, Dict
+import jwt
+from datetime import datetime, timedelta, timezone
 
 
 # Parse the cookie header as a dictionary.
@@ -9,3 +11,29 @@ def parse_cookie(cookie: str) -> Dict[str, str]:
 
     # Return the cookies.
     return cookies
+
+
+# Create a JWT from the participation code.
+def create_jwt(code: str, secret: str) -> str:
+    # If the secret is not provided.
+    if not secret:
+        # Raise.
+        raise ValueError(f"Invalid secret \"{ secret }\" provided for JWT creation.")
+
+    # Create the payload.
+    payload = {
+        # The participation code.
+        "code": code,
+
+        # Add the expiration time (i.e., three days from now).
+        "exp": datetime.now(timezone.utc) + timedelta(seconds = 259200),
+
+        # Add the issued at time.r
+        "iat": datetime.now(timezone.utc)
+    }
+
+    # Create the JWT.
+    encoded_jwt: str = jwt.encode(payload, secret, algorithm = "HS256")
+
+    # Return the encoded JWT.
+    return encoded_jwt
