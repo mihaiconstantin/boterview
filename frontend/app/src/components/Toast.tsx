a7431d19cf560d4d81d64f4196262327bb89fb6c
mihaiconstantin <mihai@mihaiconstantin.com>
2025-03-16 19:18:13 +0100

    Refactor: increase width for toast messages

--- frontend/app/src/components/Toast.tsx
+++ frontend/app/src/components/Toast.tsx
@@ -8,7 +8,7 @@ import Feedback from './Feedback';
 const Toast: React.FC<StatusMessage> = ({ status, message = {} }) => {
     // Render the toast component.
     return (
-        <div className="absolute bottom-[29px] left-1/2 transform -translate-x-1/2 text-xs border-0">
+        <div className="absolute bottom-[29px] inline-block text-xs inset-x-10 text-center border-0">
             {/* The feedback. */}
             <Feedback status={status} message={message} />
         </div>
